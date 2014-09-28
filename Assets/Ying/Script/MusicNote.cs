6f48c73e63e480f3e9f5844904d197ca145386d0
liying3 <liyingchocolate@gmail.com>
2014-09-27 21:43:36 -0400

    simple music bar

--- Assets/Ying/Script/MusicNote.cs
+++ Assets/Ying/Script/MusicNote.cs
@@ -2,23 +2,49 @@
 using System.Collections;
 
 public class MusicNote : MonoBehaviour {
-	public Vector3 orgPos;
-	public float speed;
+	Vector3 orgPos;
+	float speed;
+	
+	float hittingRange;
+	public Material blinnOrange;
 
+	public Phase1 phase1;
+	
 	// Use this for initialization
 	void Start () {
 		orgPos = gameObject.transform.position;
-		speed = 0.08f;
+		speed = 0.03f;
 
+		hittingRange = PlayerPrefs.GetFloat("ScreenWidth2World") * 0.3f * 0.25f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		gameObject.transform.position = new Vector3 (transform.position.x-speed, orgPos.y, orgPos.z);
 
-		if (gameObject.transform.position.x < PlayerPrefs.GetFloat("HittingPos"))
+		if (gameObject.transform.position.x < PlayerPrefs.GetFloat("HittingPos") + hittingRange)
 		{
-			Destroy (gameObject);
+			if (gameObject.transform.position.x < PlayerPrefs.GetFloat("HittingPos"))
+				Destroy (gameObject);
+
+			if (gameObject.renderer.material != blinnOrange)
+				gameObject.renderer.material = blinnOrange;
+
+			phase1 = GameObject.Find("Phase1").GetComponent<Phase1>();
+
+			if (gameObject.tag == "P1P1Note")
+			{
+				if (phase1.player1KeyPressDetection())
+					Destroy (gameObject);
+			}
+			if (gameObject.tag == "P1P2Note")
+			{
+				if (phase1.player2KeyPressDetection())
+					Destroy(gameObject);
+			}
 		}
 	}
+
+
+
 }
