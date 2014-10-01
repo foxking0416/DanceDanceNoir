821a7908d4b4ec4a1bc40fccbe94b131c19679c8
foxking0416 <foxking0416@gmail.com>
2014-10-01 14:24:09 -0400

    create title scene and modify the phase 2 camera

--- Assets/WeiChien/Script/Phase2/PlayerCar.cs
+++ Assets/WeiChien/Script/Phase2/PlayerCar.cs
@@ -5,6 +5,7 @@ public class PlayerCar : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		gameObject.transform.localPosition = new Vector3 (-2, 0.0f, 0.0f);
 		gameObject.transform.localRotation = Quaternion.Euler (new Vector3 (0, 90, 0));
 		gameObject.transform.localScale = new Vector3 (0.3f, 0.3f, 0.3f);
 	}
---
+++ Assets/WeiChien/Script/TitleScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnGUI (){
+		GUILayout.BeginArea(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 100, 100, 200));
+		// Load the main scene
+		// The scene needs to be added into build setting to be loaded!
+		if (GUILayout.Button("New Game")) {
+			Application.LoadLevel("GameplayScene");
+		}
+
+		if (GUILayout.Button("Introduction")) {
+			Application.LoadLevel("IntroScene");
+		}
+
+		if (GUILayout.Button("High score"))	{
+			Application.LoadLevel("HighScoreScene");
+		}
+
+		if (GUILayout.Button("Exit")) {
+			Application.Quit();
+			Debug.Log ("Application.Quit() only works in build, not in editor");
+		}
+		GUILayout.EndArea();
+	}
+}
