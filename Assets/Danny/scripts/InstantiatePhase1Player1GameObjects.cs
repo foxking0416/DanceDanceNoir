5b67b414dd9248607685a8997d93ec2959471789
Danny Rerucha <DannyRerucha@gmail.com>
2014-10-06 14:39:49 -0400

    Add player 1 gameplay to phase 1 scene.

--- Assets/Danny/scripts/InstantiatePhase1Player1GameObjects.cs
+++ Assets/Danny/scripts/InstantiatePhase1Player1GameObjects.cs
@@ -19,12 +19,14 @@ public class InstantiatePhase1Player1GameObjects : MonoBehaviour
 	////////////////////////////////////////////////////
 	void Start()
 	{
-		Vector3 cameraPosition = new Vector3( 200.0f, 0.0f, -10.0f );
-		Vector3 backgroundPosition = new Vector3( 200.0f, 0.0f, 0.0f );
-		Vector3 playerPosition = new Vector3( 200.0f, -4.0f, -1.0f );
-		Vector3 obstacleManagerPosition = new Vector3( 220.0f, 0.0f, 0.0f );
-		Vector3 obstacleGenerator1Position = new Vector3( 210.0f, -4.3f, -1.0f );
-		Vector3 obstacleGenerator2Position = new Vector3( 210.0f, -2.5f, -1.0f );
+		Vector3 basePosition = new Vector3( 400.0f, 0.0f, 0.0f );
+
+		Vector3 cameraPosition = new Vector3( basePosition.x, basePosition.y, basePosition.z - 10.0f );
+		Vector3 backgroundPosition = basePosition;
+		Vector3 playerPosition = new Vector3( basePosition.x, basePosition.y - 4.0f, basePosition.z - 1.0f );
+		Vector3 obstacleManagerPosition = new Vector3( basePosition.x + 20.0f, basePosition.y, basePosition.z );
+		Vector3 obstacleGenerator1Position = new Vector3( basePosition.x + 10.0f, basePosition.y - 4.3f, basePosition.z - 1.0f );
+		Vector3 obstacleGenerator2Position = new Vector3( basePosition.x + 10.0f, basePosition.y - 2.5f, basePosition.z - 1.0f );
 
 		Instantiate( camera, cameraPosition, transform.rotation );
 		Instantiate( background, backgroundPosition, transform.rotation );
