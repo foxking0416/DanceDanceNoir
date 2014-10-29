2f5a89bb9c67b3bd8827c58004c4f42f036c5cd7
liying3 <liyingchocolate@gmail.com>
2014-10-29 10:46:11 -0400

    add star to music bar

---
+++ Assets/Ying/Script/SpeedStar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedStar : MonoBehaviour {
+	Vector3 orgPos;
+	float speed;
+	// Use this for initialization
+	void Start () {
+		orgPos = gameObject.transform.position;
+		speed = PlayerPrefs.GetFloat("noteSpeed");
+		gameObject.transform.localScale = new Vector3 (0.08f, 0.08f, 0.08f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector3 (transform.position.x-speed, orgPos.y, orgPos.z);
+		if (gameObject.transform.position.x < PlayerPrefs.GetFloat("HittingCenter")- PlayerPrefs.GetFloat ("halfHittingRange"))
+			Destroy (gameObject);
+	}
+}
