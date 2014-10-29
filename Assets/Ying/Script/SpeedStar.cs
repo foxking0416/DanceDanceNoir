ff622e41a8fefad8193ee900e4c34c54b1c1e4e8
liying3 <liyingchocolate@gmail.com>
2014-10-29 10:49:48 -0400

    prefab edit

--- Assets/Ying/Script/SpeedStar.cs
+++ Assets/Ying/Script/SpeedStar.cs
@@ -8,7 +8,6 @@ public class SpeedStar : MonoBehaviour {
 	void Start () {
 		orgPos = gameObject.transform.position;
 		speed = PlayerPrefs.GetFloat("noteSpeed");
-		gameObject.transform.localScale = new Vector3 (0.08f, 0.08f, 0.08f);
 	}
 	
 	// Update is called once per frame
